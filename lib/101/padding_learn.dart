52006139dee1c94ebe06ede1970a4fc2c848e9e0
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2022-04-28 15:01:36 +0300

    padding

---
+++ lib/101/padding_learn.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PaddingLearn extends StatelessWidget {
+  const PaddingLearn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: ProjectPadding.pagePaddingHorizantal,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(color: Colors.white, height: 100),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Container(color: Colors.white, height: 100),
+            ),
+            const Padding(
+              padding: ProjectPadding.pagePaddingTopOnly,
+              child: Text('data'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProjectPadding {
+  static const pagePaddingHorizantal = EdgeInsets.symmetric(horizontal: 30);
+  static const pagePaddingTopOnly = EdgeInsets.only(top: 30);
+}
